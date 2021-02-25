ba74e3b839813c55fe677c47890eb9e68efeee70
cyx299-111 <1457132801@qq.com>
2021-02-25 23:12:59 +0800

    feat: separate phrases

---
+++ TX_fenci.py
@@ -0,0 +1,35 @@
+import jieba
+import json
+
+
+txt = open("comments.txt", "r", encoding='utf-8').read()
+words = jieba.lcut(txt)
+
+dataDict = {}
+countList = []
+
+
+# 统计词语出现次数
+counts = {}
+
+for word in words:
+     if len(word) == 1:
+         continue
+     else:
+        counts[word] = counts.get(word,0) + 1  
+items = list(counts.items())
+items.sort(key=lambda x:x[1], reverse=True)
+
+
+for i in range(len(items)):
+    countDict = {}
+    word, count = items[i]
+    if count >= 10:
+        countDict['name'] = word
+        countDict['value'] = count
+        countList.append(countDict)
+
+dataDict['data'] = countList
+print(dataDict)
+with open('comments.json', 'w', encoding='utf-8') as f:
+    json.dump(dataDict, f, ensure_ascii=False, indent=4)
